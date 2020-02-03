5dfa36914e1aee818eab557548203154f65acf54
Noboru Matsumoto <matsumoto@noborunoMacBook-Pro.local>
2020-02-03 15:42:53 +0900

    arrange setup_env.bash

---
+++ setup_env.bash
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# required packages
+
+if [ "$(uname)" == "Darwin" ]; then
+    os=mac
+    if !(type curl > /dev/null 2>&1);then 
+        brew install curl 
+    fi
+elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
+    os=windows
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    os=linux
+    if !(type curl > /dev/null 2>&1);then 
+        sudo apt install curl
+    fi
+else
+    os=unknown
+fi
+
+# download install script and execute it
+mkdir -p ~/.vim/.cache/dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
+sh installer.sh ~/.vim/.cache/dein
+rm installer.sh
+
+# link dotfiles
+if [ $os = "mac" ] || [ $os = "linux" ]; then
+    ln -sf ~/dotfiles/.vimrc     ~/.vimrc
+    ln -sf ~/dotfiles/.gvimrc    ~/.gvimrc
+    ln -sf ~/dotfiles/.zshrc     ~/.zshrc
+    ln -sf ~/dotfiles/.unixenv   ~/.unixenv
+    ln -sf ~/dotfiles/.macenv    ~/.macenv
+    ln -sf ~/dotfiles/select_termenv ~/select_termenv
+elif [ $os = "MINGW" ]; then
+    ln -sf ~/dotfiles/.vimrc     ~/_vimrc
+    ln -sf ~/dotfiles/.gvimrc    ~/_gvimrc
+fi
+ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
+
