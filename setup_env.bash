b6e70d728392e7a278224f08006dece0e64bee16
matsumoto@letsnote <matsumoto.idr@gmail.com>
2020-04-10 20:58:58 +0900

    improved setup_env.bash to setup virtualenvwrapper

--- setup_env.bash
+++ setup_env.bash
@@ -16,7 +16,30 @@ elif [ $os = "linux" ] ; then
     cat ./env/ubuntu16/packages | xargs sudo apt install -y
     cat ./env/default_python_packages | xargs -L 1 sudo pip3 install 
 fi
-exit 0
+
+# install virtualenvwrapper
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
+    export WORKON_HOME=$HOME/.venvs
+    source /usr/local/bin/virtualenvwrapper.sh
+    if [ $? != 0 ]; then
+        echo "setup virtualenvwrapper failed! abort"
+        exit 1
+    fi
+
+    if [[ $SHELL = *"/bash" ]] ; then initshell=.bashrc
+    elif [[ $SHELL = *"/zsh" ]]; then initshell=.zshrc
+    fi
+
+    grep "export WORKON_HOME=" $HOME/$initshell
+    if [ $? != 0 ];then
+        echo "if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then" >> $HOME/$initshell
+        echo "  export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3"  >> $HOME/$initshell
+        echo "  export WORKON_HOME=$HOME/.venvs"                   >> $HOME/$initshell
+        echo "  source /usr/local/bin/virtualenvwrapper.sh"        >> $HOME/$initshell
+        echo "fi" >> $HOME/$initshell
+    fi
+fi
 
 read -p "Do you install fonts? (y/N): " yn
 case "$yn" in
@@ -34,18 +57,31 @@ esac
 
 if [ $os = "linux" ]; then
     # fix time difference in dual boot env
-    timedatectl set-local-rtc true
+    read -p "is thie dualboot env?[y/N]" ys
+    case $ys in 
+        [yY]*) 
+            timedatectl set-local-rtc true 
+            echo "Done fixing time setting"
+            ;;
+        *) echo "skip setting time problem between windows and linux";;
+    esac
+
+    echo "setting font and theme..."
+    unity-tweak-tool -a
+    echo "Done"
 
     # regard caps as ctrl
+    echo "Setting caps as ctrl"
     gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']"
+    echo "Done"
 
     # for backlight on Let's note
+    echo "Change boot setting..."
     sudo sed --in-place 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash"/GRUB_CMDLINE_LINUX_DEFAULT="quiet splash acpi_backlight=vendor acpi_osi="/g' /etc/default/grub
     sudo sed --in-place 's/GRUB_TIMEOUT=10/GRUB_TIMEOUT=300/g' /etc/default/grub
-
-    unity-tweak-tool -a
 
     # update grub
     sudo update-initramfs -u
     sudo update-grub
+    echo "Done updatting grub with new config"
 fi
