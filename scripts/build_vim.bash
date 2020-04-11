2e513e4b0ec6c4b8fdac4dc874a4517fc2cda754
matsumoto <matsumoto@ts01.idr.ias.sci.waseda.ac.jp>
2020-04-11 22:59:37 +0900

    improved scripts

--- scripts/build_vim.bash
+++ scripts/build_vim.bash
@@ -1,5 +1,4 @@
 #!/env/bash
-#TODO
 
 PREFIX=$HOME/.local
 vim_ver="8.2"
@@ -17,66 +16,87 @@ elif [ "$(expr substr $(uname -s) 1 5)" = "Linux" ]; then os=linux
 else os=unknown
 fi
 
-echo "Installing lua..."
-cd $src_dir
-wget https://www.lua.org/ftp/lua-$lua_ver.tar.gz 
-tar zxvf lua-$lua_ver.tar.gz 
-cd lua-$lua_ver
-if [ $os = "mac" ];then
-    sed -i .bak "s/INSTALL_TOP= \/usr\/local/INSTALL_TOP= \$\(HOME\)\/.local\/lua-$lua_ver/" Makefile
-    make macosx
-elif [ $os = "linux" ]; then
-    sed -i "s/INSTALL_TOP= \/usr\/local/INSTALL_TOP= \$\(HOME\)\/.local\/lua-$lua_ver/" Makefile
-    make linux
-fi
-make install
-if [ $? != 0 ];then
-    echo "installing lua failed! abort..."
-    exit 1
-else
-    echo "Successfully installed lua"
-fi
-
-echo "Install ncurses..."
-cd $src_dir
-wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-$ncurses_ver.tar.gz
-tar xvfz ncurses-$ncurses_ver.tar.gz
-cd ncurses-$ncurses_ver
-./configure --prefix=$HOME/.local/ncurses-$ncurses_ver --with-shared --with-pkg-config-libdir=$HOME/.local/ncurses-$ncurses_ver/lib/pkgconfig --enable-pc-files
-make
-make install
-if [ $? != 0 ];then
-    echo "installing ncurses failed! abort..."
-    exit 1
-else
-    echo "Successfully installed ncurses"
+if [ ! -e $HOME/.local/lua-$lua_ver ]; then
+    echo "Installing lua..."
+    cd $src_dir
+    if [ ! -e ./lua-$lua_ver ]; then
+        if [ ! -e ./lua-$lua_ver.tar.gz ]; then
+            wget https://www.lua.org/ftp/lua-$lua_ver.tar.gz -t 10
+        fi
+        tar zxvf lua-$lua_ver.tar.gz 
+    fi
+    cd lua-$lua_ver
+    if [ $os = "mac" ];then
+        sed -i .bak "s/INSTALL_TOP= \/usr\/local/INSTALL_TOP= \$\(HOME\)\/.local\/lua-$lua_ver/" Makefile
+        make macosx && make install
+    elif [ $os = "linux" ]; then
+        sed -i "s/INSTALL_TOP= \/usr\/local/INSTALL_TOP= \$\(HOME\)\/.local\/lua-$lua_ver/" Makefile
+        make linux && make install
+    fi
+    if [ $? != 0 ];then
+        echo "installing lua failed! abort..."
+        exit 1
+    else
+        echo "Successfully installed lua"
+    fi
 fi
 
+if [ ! -e $HOME/.local/ncurses-$ncurses_ver ]; then
+    echo "Install ncurses..."
+    cd $src_dir
+    if [ ! -e ./ncurses-$ncurses_ver ]; then
+        if [ ! -e ./ncurses-$ncurses_ver.tar.gz ]; then
+            wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-$ncurses_ver.tar.gz -t 10
+        fi
+        tar xvfz ncurses-$ncurses_ver.tar.gz
+    fi
+    cd ncurses-$ncurses_ver
+    ./configure --prefix=$HOME/.local/ncurses-$ncurses_ver --with-shared --with-pkg-config-libdir=$HOME/.local/ncurses-$ncurses_ver/lib/pkgconfig --enable-pc-files
+    make && make install
+    if [ $? != 0 ];then
+        echo "installing ncurses failed! abort..."
+        exit 1
+    else
+        echo "Successfully installed ncurses"
+    fi
+fi
+
 cd $src_dir
-wget ftp://ftp.vim.org/pub/vim/unix/vim-$vim_ver.tar.bz2 -O vim-$vim_ver.tar.bz2
-mkdir vim-$vim_ver
-tar jxvf vim-$vim_ver.tar.bz2 -C vim-$vim_ver --strip-components=1
+if [ ! -e ./vim-$vim_ver ]; then
+    if [ ! -e ./vim-$vim_ver.tar.bz2 ]; then
+        wget --no-check-certificate --tries=10 --retry-connrefused --waitretry=1 --timeout=10 https://ftp.vim.org/pub/vim/unix/vim-$vim_ver.tar.bz2 -O vim-$vim_ver.tar.bz2
+    fi
+    mkdir vim-$vim_ver
+    tar jxvf vim-$vim_ver.tar.bz2 -C vim-$vim_ver --strip-components=1
+    if [ $? != 0 ]; then
+        rm -rf vim-$vim_ver vim-$vim_ver.tar.bz2
+    fi
+fi
+
 cd vim-$vim_ver
-
+<< com
 ./configure \
     --prefix=$PREFIX\
     --with-features=huge \
+    --with-local-dir=$HOME/.local \
     --enable-multibyte \
-    --enable-gui=auto\
+    --enable-gui=auto \
     --enable-pythoninterp=yes \
+    --enable-python3interp=yes \
     --enable-luainterp=yes \
-    --enable-perlinterp=yes \
+    --enable-rubyinterp=yes\
+    --enable-perlinterp=auto \
     --with-lua-prefix=$HOME/.local/lua-$lua_ver \
     --enable-cscope \
     --enable-fontset \
     --enable-fail-if-missing \
-    #--enable-rubyinterp=yes\
     #--enable-tclinterp=yes\
+
+com
 
 if [ $? != 0 ]; then
     echo "error configurinig. abort"
     exit 1
 else
-    make
-    make install
+    make && make install
 fi
