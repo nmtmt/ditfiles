1c4d44949dd643555beb4b75f066634a2f54ad51
matsumoto@aistserver <matsumoto.idr@gmail.com>
2020-05-03 01:24:18 +0900

    add ncurses path in tmux.bash

--- scripts/build_info/tmux.bash
+++ scripts/build_info/tmux.bash
@@ -4,6 +4,7 @@ url=https://github.com/tmux/tmux/releases/download/$ver/tmux-$ver.tar.gz
 pkg_tarname=tmux-$ver.tar.gz
 pkg_dirname=tmux-$ver
 commands(){
-    ./configure --prefix=$HOME/.local --enable-shared --enable-static
+    ncurses=$(find $HOME/.local -maxdepth 1 -name "ncurses-*" -type d)
+    ./configure --prefix=$HOME/.local --enable-shared --enable-static CFLAGS="-I$ncurses/include/ncurses -I$ncurses/include" LDFLAGS="-L$ncurses/lib"
     make -j4 && make install
 }
