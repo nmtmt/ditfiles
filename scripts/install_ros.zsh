57a7b2c6da130dd187c21a136a59399efbd5591e
Matsumoto Noboru <matsumoto.idr@gmail.com>
2020-09-09 01:50:28 +0900

    fixed typo of package

--- scripts/install_ros.zsh
+++ scripts/install_ros.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 release=$(cat /etc/lsb-release | grep RELEASE | cut -d '=' -f 2)
@@ -13,5 +13,8 @@ sudo apt install ros-$distro-desktop-full ros-$distro-joint-state-publisher-gui 
 sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
 sudo rosdep init
 rosdep update
+source $HOME/.zshrc
 mkvirtualenv ros --python=python2
-pip install pyymal rospkg empy defusedxml numpy scipy matplotlib pycryptodomex gnupg
+if [ $? = 0 ];then
+    pip install pyyaml rospkg empy defusedxml numpy scipy matplotlib pycryptodomex gnupg
+fi
